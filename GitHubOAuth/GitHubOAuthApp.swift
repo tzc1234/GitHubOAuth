72232e76dc8ff9f1ea8fe94c4e5d90e00cbe298b
tzc1234 <peopli9997@gmail.com>
2024-10-25 10:00:25 +0100

    Initial Commit

---
+++ GitHubOAuth/GitHubOAuthApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubOAuthApp.swift
+//  GitHubOAuth
+//
+//  Created by Tsz-Lung on 25/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct GitHubOAuthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
